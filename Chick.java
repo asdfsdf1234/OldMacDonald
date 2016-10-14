49ee33c387817ddc3d9a4be3fa96763693b6aa26
asdfsdf1234 <JaAngeles@s.sfusd.edu>
2016-10-14 11:10:31 -0700

    taking Ls for a living

--- Chick.java
+++ Chick.java
@@ -1,4 +1,18 @@
 class Chick implements Animal 
 {     
-     //your code here
+    private String myType;     
+    private String mySound; 
+    private String x;     
+    public Chick(String type, String sound, String sond){         
+        myType = type;         
+        mySound = sound; 
+        x=sond;    
+    }   
+    public String getSound(){
+    	if(Math.random()<=.5){
+    		return x;
+    	}
+    	else return mySound;
+    }     
+    public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,7 @@
 
-void setup()
-{
-	//your variable declarations here
-}
+public void setup() {     
+  Farm one = new Farm();
+  one.animalSounds();
+}  
+
 
